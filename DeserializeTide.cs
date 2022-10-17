ba0d2d2d72c9b581c8be54f708c5514fa7874446
Hariyadi Nasution <86828223+Dayyadi@users.noreply.github.com>
2022-10-18 05:11:12 +0700

    Add files via upload

---
+++ DeserializeTide.cs
@@ -0,0 +1,28 @@
+﻿namespace WeatherTideStormglass
+{
+    internal class DeserializeTide
+    {
+        public _data[] data { get; set; }
+        public _meta meta { get; set; }
+    }
+    public class _data
+    {
+        public string height { get; set; }
+        public string time { get; set; }
+        public string type { get; set; }
+    }
+
+    public class _meta
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+        public _station station { get; set; }
+    }
+    public class _station
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string name { get; set; }
+        public string source { get; set; }
+    }
+}
---
+++ WebRequest .cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace WeatherTideStormglass
+{
+    internal class WebRequestData
+    {
+        public DeserializeTide _tide { get; set; }
+        public DeserializeWeather _weather { get; set; }
+        public WebRequestData()
+        {
+            const string lat = "lat=" + "-6.200000";
+            const string lng = "lng=" + "106.816666";
+            const string WEBSERVICE_URL_TIDE = "https://api.stormglass.io/v2/tide/extremes/point?" + lat + "&" + lng;
+            const string WEBSERVICE_URL_WEATHER = "https://api.stormglass.io/v2/weather/point?" + lat + "&" + lng + "&params=airTemperature";
+            try
+            {
+                WebRequest webRequestTide = WebRequest.Create(WEBSERVICE_URL_TIDE);
+                WebRequest webRequestWeather = WebRequest.Create(WEBSERVICE_URL_WEATHER);
+
+                if (webRequestTide != null && webRequestWeather != null)
+                {
+                    webRequestTide.Method = "GET";
+                    webRequestTide.Timeout = 12000;
+                    webRequestTide.ContentType = "application/json";
+                    webRequestTide.Headers.Add("Authorization", "97878a8e-2a46-11ec-bf3f-0242ac130002-97878b88-2a46-11ec-bf3f-0242ac130002");
+
+                    webRequestWeather.Method = "GET";
+                    webRequestWeather.Timeout = 12000;
+                    webRequestWeather.ContentType = "application/json";
+                    webRequestWeather.Headers.Add("Authorization", "97878a8e-2a46-11ec-bf3f-0242ac130002-97878b88-2a46-11ec-bf3f-0242ac130002");
+
+                    Stream st = webRequestTide.GetResponse().GetResponseStream();
+                    StreamReader str = new StreamReader(st);
+
+                    Stream sw = webRequestWeather.GetResponse().GetResponseStream();
+                    StreamReader swr = new StreamReader(sw);
+
+                    var jsonResponseTide = str.ReadToEnd();
+                    var dataTide = JsonConvert.DeserializeObject<DeserializeTide>(jsonResponseTide);
+                    _tide = dataTide;
+
+                    var jsonResponseWeather = swr.ReadToEnd();
+                    var dataWeather = JsonConvert.DeserializeObject<DeserializeWeather>(jsonResponseWeather);
+                    _weather = dataWeather;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public DeserializeTide returnDataTide()
+        {
+            return _tide;
+        }
+    }
+}
